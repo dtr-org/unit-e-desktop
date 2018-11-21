63954725ad2ae80a5b4bf3a934a91da7e88be6a1
Cornelius Schumacher <cornelius@thirdhash.com>
2018-11-21 15:18:01 +0100

    Add Unit-e copyright notice
    This adds the Unit-e copyright notice on the files we have touched
    after the initial import.
    
    Signed-off-by: Cornelius Schumacher <cornelius@thirdhash.com>

--- src/app/wallet/wallet/send/send.service.ts
+++ src/app/wallet/wallet/send/send.service.ts
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017-2018 The Particl developers
+ * Copyright (C) 2018 The Unit-e developers
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
